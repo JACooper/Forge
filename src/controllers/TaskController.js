eb489946d0c6c61f30d4a65eb84f90d1e2e89948
Jesse Cooper <JesseAndCooper@Gmail.com>
2017-09-24 10:46:49 -0400

    Removed unused code

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -36,40 +36,6 @@ const createTask = (_request, _response) => {
     });
 };
 
-// const createTask = (_request, _response) => {
-//   const request = _request;
-//   const response = _response;
-
-//   const title = request.body.task.title;
-//   const time = request.body.task.time;
-//   const effort = request.body.task.effort;
-//   const focus = request.body.task.focus;
-  
-//   const category = request.body.task.categoryID;
-
-//   Category.findById(category)
-//     .exec()
-//     .then((category) => {
-//       const taskData = {
-//         user: request.user._id,
-//         category,
-//         title,
-//         time,
-//         effort,
-//         focus,
-//       };
-//       const newTask = new Task(taskData);
-//       return newTask.save();
-//     })
-//     .then((savedTask) => {
-//       response.status(200).json({task: savedTask});
-//     })
-//     .catch((error) => {
-//       console.dir(error);
-//       response.status(400).json({error: 'An error occurred creating the task'});
-//     });
-// };
-
 const getTasks = (_request, _response) => {
   const request = _request;
   const response = _response;
